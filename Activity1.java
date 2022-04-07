52560bad105188f8bc0a50ef2f717963591af2b7
aby-ust <198610@ust-global.com>
2022-04-07 13:20:13 +0530

    First commit

---
+++ Activity1.java
@@ -0,0 +1,15 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		
+         WebDriver driver = new ChromeDriver();
+         driver.get("http://www.google.com");
+         driver.quit();
+	}
+
+}
